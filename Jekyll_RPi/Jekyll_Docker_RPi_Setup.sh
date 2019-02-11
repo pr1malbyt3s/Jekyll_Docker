03ea47997268a95811b1b0933eefafcd31472bce
pr1malbyt3s <al33w6@gmail.com>
2019-02-11 12:56:43 -0500

    Update Jekyll_Docker_RPi_Setup.sh

--- Jekyll_RPi/Jekyll_Docker_RPi_Setup.sh
+++ Jekyll_RPi/Jekyll_Docker_RPi_Setup.sh
@@ -29,9 +29,8 @@ chown -R $dir_owner:$dir_owner .
 echo -e "${GREEN}Starting Jekyll container.${NC}"
 docker run --rm -v $PWD:/srv/jekyll -it pr1malbyt3s/jekyll_rpi new .
 sleep 3
-rm Gemfile.lock
 docker run --rm -v $PWD:/srv/jekyll -it pr1malbyt3s/jekyll_rpi build
 sleep 3
-docker run --name jekyll_jungle -v $PWD:/srv/jekyll -d -p 127.0.0.1:3000:4000 pr1malbyt3s/jekyll_rpi serve --watch --drafts --incremental
+docker run --name jekyll_jungle -v $PWD:/srv/jekyll -d --network=host pr1malbyt3s/jekyll_rpi serve --watch --drafts --incremental
 sleep 3
 chown -R $dir_owner:$dir_owner .
